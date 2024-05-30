c20460e91192db26c47de7574e4911eda4fd232c
Kotografiya <168893748+Kotografiya@users.noreply.github.com>
2024-05-30 20:13:43 +0600

    wswswsws

--- app/api/config.js
+++ app/api/config.js
@@ -1,6 +1,6 @@
 export const BASE_URL = 'https://kotografia.nomoredomainswork.ru/api'
 export const endpoints = {
-  games: `${BASE_URL}/games`,
+  games: `${BASE_URL}/api/games`,
   auth: `${BASE_URL}/auth/login`,
   me: `${BASE_URL}/me`,
 }
